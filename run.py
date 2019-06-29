ec4dfc693ce9d2f0bcccea72b86dc7804b9186ad
thandonoganta <ngntha001@myuct.ac.za>
2019-06-29 19:45:18 +0200

    remove if_name__ == 'main' for deployment prep

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from app import create_app,db
 from app.auth.models import User
 
-if __name__ == '__main__':
-    flask_app = create_app('prod')
-    with flask_app.app_context():
-        db.create_all()
+##if __name__ == '__main__':
+flask_app = create_app('prod')
+with flask_app.app_context():
+    db.create_all()
     flask_app.run()
