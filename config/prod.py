e40fc7a33ac0f0ccf4dc87d5fac335e8209aa2b5
thandonoganta <ngntha001@myuct.ac.za>
2019-06-29 19:50:24 +0200

    mod config for deployment preperation

--- config/prod.py
+++ config/prod.py
@@ -2,6 +2,6 @@ DEBUG = False
 
 SECRET_KEY = 'PanxuPanxu7671$'
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:PanxuPanxu7671$@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = as.environ['DATABASE_URL']
 
 SQLALCHEMY_DATABASE_MODIFICATIONS = False
