f59c66bd106f1595ba54621a227cf16082e3a66b
thandonoganta <ngntha001@myuct.ac.za>
2019-06-29 20:56:42 +0200

    runtime error - database integrity

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 from app import create_app,db
 from app.auth.models import User
+from sqlalchemy import exc
 
-##if __name__ == '__main__':
+#if __name__ == '__main__':
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    flask_app.run()
+    try:
+        if not User.query.filter_by(user_name ='Thando').first():
+            User.create_user(user='Thando', email='ngntha001@myuct.ac.za', password='PanxuPanxu7671$')
+    except exc.IntegrityError:
+            flask_app.run()
