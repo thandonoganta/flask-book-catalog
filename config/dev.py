8015f0139531412cf1b9f00656dadd55e608d5fd
thandonoganta <ngntha001@myuct.ac.za>
2019-06-29 19:35:33 +0200

    first commit

---
+++ config/dev.py
@@ -0,0 +1,7 @@
+DEBUG = True
+
+SECRET_KEY = 'PanxuPanxu7671$'
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:PanxuPanxu7671$@localhost/catalog_db'
+
+SQLALCHEMY_DATABASE_MODIFICATIONS = False
---
+++ config/prod.py
@@ -0,0 +1,7 @@
+DEBUG = False
+
+SECRET_KEY = 'PanxuPanxu7671$'
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:PanxuPanxu7671$@localhost/catalog_db'
+
+SQLALCHEMY_DATABASE_MODIFICATIONS = False
