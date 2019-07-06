a3387667d2bf89cac6798fc5d13ed9fb88371a72
thandonoganta <ngntha001@myuct.ac.za>
2019-07-06 22:28:49 +0200

    prep for deployment: letting heroku know that we want to run a web app using the gunicorn http server

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from app.auth.models import User
 
 if __name__ == '__main__':
-    flask_app = create_app('test')
+    flask_app = create_app('prod')
     with flask_app.app_context():
         db.create_all()
     flask_app.run()
