e404d2f007e858b7e497127074ead357d14bde72
thandonoganta <ngntha001@myuct.ac.za>
2019-07-08 20:45:12 +0200

    add try-except block for database integrity error

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app,db
 from app.auth.models import User
+from sqlalchemy import exc
 
 # if __name__ == '__main__':
 # #     flask_app = create_app('prod')
@@ -7,9 +8,20 @@ from app.auth.models import User
 # #         db.create_all()
 # #     flask_app.run()
 
+# flask_app = create_app('prod')
+# with flask_app.app_context():
+#     db.create_all()
+#     if not User.query.filter_by(user_name = 'Thando').first():
+#         User.create_user(user='Thando', email='ngntha001@myuct.ac.za', password='PanxuPanxu7671$')
+#         flask_app.run()
+
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name = 'Thando').first():
-        User.create_user(user='Thando', email='ngntha001@myuct.ac.za', password='PanxuPanxu7671$')
-        flask_app.run()
+    try:
+        if not User.query.filter_by(user_name = 'Thando').first():
+            User.create_user(user='Thando', email='ngntha001@myuct.ac.za'
+                             ,password='PanxuPanxu7671$')
+    except: exc.IntegrityError
+
+            flask_app.run()
